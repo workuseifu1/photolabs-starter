4c438bfd69d5ca773176bd61892ba4e027c1a690
workuseifu1 <workuseifu1@gmail.com>
2023-08-23 12:12:56 -0500

    add photoListItem component and add some styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
 
+// import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
+import './styles/PhotoListItem.scss';
+// import './App.scss';
 import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
+const sampleDataForPhotoListItem = [{
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  liked: true
+},
+{
+  id: "2",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  liked: true
+},
+{
+  id: "3",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  liked: true
+}
+];
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
-    <div className="App">
-      <PhotoListItem/>
+    <div className='app'>
+      {sampleDataForPhotoListItem.map(photo => (
+        <PhotoListItem key={photo.id} {...photo} />
+      ))}
+      {/* <PhotoListItem  {...sampleDataForPhotoListItem}/> */}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,20 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  const { username, imageSource, id, location, profile , liked} = props;
+  return (
+    <div className ="photo-list__item">
+      <img src={imageSource} alt={`Photo by ${username}` } className = "photo-list__image"  />
+      <div className = "photo-list__user-info">
+        <h3 className = "photo-list__user-details" >{username}</h3>
+        <p className="photo-list__user-location">{`${location.city}, ${location.country}`}</p>
+        <p className="liked">{liked ? 'Liked' : 'Not Liked'}</p>
+      </div>
+      <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
+    </div>
+  );
 };
 
 export default PhotoListItem;
